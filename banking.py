5c4c71ef0504d43f167992b655178d6941c2325a
VicSoumina <99531339+VicSoumina@users.noreply.github.com>
2022-03-24 18:58:35 +0300

    Update banking.py

--- banking.py
+++ banking.py
@@ -1,8 +1,6 @@
-money = int(input('Сумма под проценты: '))
-per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-deposit = []
-for key in per_cent:
-    deposit.append(round(money * per_cent[key] / 100))
-max_sum = max(deposit)
-print(deposit)
-print('Максимальная сумма, которую вы можете заработать: ', max_sum)
+n = 5
+counter = 1
+
+for i in range(1, n+1):
+    counter *=i
+print(counter)    
